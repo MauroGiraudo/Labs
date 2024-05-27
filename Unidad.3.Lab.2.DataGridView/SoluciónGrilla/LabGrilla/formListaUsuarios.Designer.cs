a76985e816bc80f89e9718bbf0ef6e14b3059735
Mauro F. Giraudo <mfgiraudo456@gmail.com>
2024-05-27 13:27:19 -0300

    Laboratorio Terminado con Errores (_usuario no reconocido). CONSULTAR

---
+++ Unidad.3.Lab.2.DataGridView/SoluciónGrilla/LabGrilla/formListaUsuarios.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace LabGrilla
+{
+    partial class formListaUsuarios
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tipo_doc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nro_doc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fecha_nac = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lista de Usuarios: ";
+            // 
+            // dgvUsuarios
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Cyan;
+            this.dgvUsuarios.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvUsuarios.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.apellido,
+            this.tipo_doc,
+            this.nro_doc,
+            this.nombre,
+            this.fecha_nac,
+            this.direccion});
+            this.dgvUsuarios.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dgvUsuarios.Location = new System.Drawing.Point(15, 28);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.RowHeadersWidth = 51;
+            this.dgvUsuarios.RowTemplate.Height = 24;
+            this.dgvUsuarios.Size = new System.Drawing.Size(577, 390);
+            this.dgvUsuarios.TabIndex = 1;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGuardar.Location = new System.Drawing.Point(505, 424);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(87, 36);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // apellido
+            // 
+            this.apellido.HeaderText = "Apellido";
+            this.apellido.MinimumWidth = 6;
+            this.apellido.Name = "apellido";
+            this.apellido.Width = 125;
+            // 
+            // tipo_doc
+            // 
+            this.tipo_doc.HeaderText = "Tipo de Documento";
+            this.tipo_doc.MinimumWidth = 6;
+            this.tipo_doc.Name = "tipo_doc";
+            this.tipo_doc.Width = 125;
+            // 
+            // nro_doc
+            // 
+            this.nro_doc.HeaderText = "Nro. Documento";
+            this.nro_doc.MinimumWidth = 6;
+            this.nro_doc.Name = "nro_doc";
+            this.nro_doc.Width = 125;
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.MinimumWidth = 6;
+            this.nombre.Name = "nombre";
+            this.nombre.Width = 125;
+            // 
+            // fecha_nac
+            // 
+            this.fecha_nac.HeaderText = "Fecha de Nacimiento";
+            this.fecha_nac.MinimumWidth = 6;
+            this.fecha_nac.Name = "fecha_nac";
+            this.fecha_nac.Width = 125;
+            // 
+            // direccion
+            // 
+            this.direccion.HeaderText = "Dirección";
+            this.direccion.MinimumWidth = 6;
+            this.direccion.Name = "direccion";
+            this.direccion.Width = 125;
+            // 
+            // formListaUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 472);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dgvUsuarios);
+            this.Controls.Add(this.label1);
+            this.Name = "formListaUsuarios";
+            this.Text = "Usuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvUsuarios;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tipo_doc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nro_doc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fecha_nac;
+        private System.Windows.Forms.DataGridViewTextBoxColumn direccion;
+    }
+}
+
