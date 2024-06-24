7c92181c889750aad88c4c494793e4b324b848cf
Mauro F. Giraudo <mfgiraudo456@gmail.com>
2024-06-24 14:12:09 -0300

    Unidad4Cap1Lab02 Completed

---
+++ Unidad.4.Lab.2.ArchivosTXTyXMLconADO.Net/SoluciónAdo.Net/Ado.Net/ManejadorArchivoTxt.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace Ado.Net
+{
+    internal class ManejadorArchivoTxt : ManejadorArchivo
+    {
+        protected string connectionString
+        {
+            get
+            {
+                return @"Provider=Microsoft.Jet.OLEDB.4.0; 
+                Data Source=D:\Mauro\Colegio_Facultad\UTN\3ero\Tecnologías_de_Desarrollo_de_Software_IDE_(.NET)\Labs\Unidad.4.Lab.2.ArchivosTXTyXMLconADO.Net\SoluciónAdo.Net\Ado.Net\bin\Debug;" +
+                "Extended Properties='text;HDR=Yes;FMT=Delimited'";
+            }
+        }
+
+        public override DataTable getTabla()
+        {
+            using (OleDbConnection Conn = new OleDbConnection(connectionString))
+            {
+                OleDbCommand cmdSelect = new OleDbCommand("select * from agenda.txt", Conn);
+                Conn.Open();
+                OleDbDataReader reader = cmdSelect.ExecuteReader();
+                DataTable contactos = new DataTable();
+                if (reader != null)
+                {
+                    contactos.Load(reader);
+
+                }
+
+                Conn.Close();
+                return contactos;
+            }
+        }
+
+        public override void aplicaCambios()
+        {
+            using (OleDbConnection Conn = new OleDbConnection(connectionString))
+            {
+                OleDbCommand cmdInsert = new OleDbCommand("insert into agenda.txt values (@id,@nombre,@apellido,@email,@telefono)", Conn);
+                cmdInsert.Parameters.Add("@id", OleDbType.Integer);
+                cmdInsert.Parameters.Add("@nombre", OleDbType.VarChar);
+                cmdInsert.Parameters.Add("@apellido", OleDbType.VarChar);
+                cmdInsert.Parameters.Add("@email", OleDbType.VarChar);
+                cmdInsert.Parameters.Add("@telefono", OleDbType.VarChar);
+
+                OleDbCommand cmdUpdate = new OleDbCommand("update agenda.txt set nombre=@nombre, apellido=@apellido, email=@email, telefono=@telefono where id=@id", Conn);
+                cmdUpdate.Parameters.Add("@id", OleDbType.Integer);
+                cmdUpdate.Parameters.Add("@nombre", OleDbType.VarChar);
+                cmdUpdate.Parameters.Add("@apellido", OleDbType.VarChar);
+                cmdUpdate.Parameters.Add("@email", OleDbType.VarChar);
+                cmdUpdate.Parameters.Add("@telefono", OleDbType.VarChar);
+
+                OleDbCommand cmdDelete = new OleDbCommand("delete from agenda.txt where id=@id", Conn);
+                cmdDelete.Parameters.Add("@id", OleDbType.Integer);
+
+                DataTable filasNuevas = this.misContactos.GetChanges(DataRowState.Added);
+                DataTable filasModificadas = this.misContactos.GetChanges(DataRowState.Modified);
+                DataTable filasEliminadas = this.misContactos.GetChanges(DataRowState.Deleted);
+
+                Conn.Open();
+
+                if (filasNuevas != null)
+                {
+                    foreach (DataRow fila in filasNuevas.Rows)
+                    {
+                        cmdInsert.Parameters["@id"].Value = fila["id"];
+                        cmdInsert.Parameters["@nombre"].Value = fila["nombre"];
+                        cmdInsert.Parameters["@apellido"].Value = fila["apellido"];
+                        cmdInsert.Parameters["@email"].Value = fila["email"];
+                        cmdInsert.Parameters["@telefono"].Value = fila["telefono"];
+                        cmdInsert.ExecuteNonQuery();
+                    }
+                }
+
+                if (filasModificadas !=  null)
+                {
+                    foreach(DataRow fila in filasModificadas.Rows)
+                    {
+                        cmdUpdate.Parameters["@id"].Value = fila["id"];
+                        cmdUpdate.Parameters["@nombre"].Value = fila["nombre"];
+                        cmdUpdate.Parameters["@apellido"].Value = fila["apellido"];
+                        cmdUpdate.Parameters["@email"].Value = fila["email"];
+                        cmdUpdate.Parameters["@telefono"].Value = fila["telefono"];
+                        cmdUpdate.ExecuteNonQuery();
+                    }
+                }
+
+                if (filasEliminadas != null)
+                {
+                    foreach(DataRow fila in filasEliminadas.Rows)
+                    {
+                        cmdDelete.Parameters["@id"].Value = fila["id", DataRowVersion.Original];
+                        cmdDelete.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+}
