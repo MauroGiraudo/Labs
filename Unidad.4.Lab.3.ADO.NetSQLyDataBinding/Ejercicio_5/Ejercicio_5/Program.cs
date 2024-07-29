8c393c841b7fb1679794374c32656040b0716016
Mauro F. Giraudo <mfgiraudo456@gmail.com>
2024-07-29 13:43:07 -0300

    Laboratorios Ejercicios del laboratorio 4.3 Finalizados (REVISAR PARA CONSULTAS POR ACCESO A LA BASE DE DATOS)

---
+++ Unidad.4.Lab.3.ADO.NetSQLyDataBinding/Ejercicio_5/Ejercicio_5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ejercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dtEmpresas = new DataTable();
+            dtEmpresas.Columns.Add("CustomerID", typeof(string));
+            dtEmpresas.Columns.Add("CompanyName", typeof(string));
+            SqlConnection myconn = new SqlConnection();
+            myconn.ConnectionString = "Data Source=LOCALHOST;Initial Catalog=Northwind; User ID=sa;Pwd=123";
+            SqlDataAdapter myAdap = new SqlDataAdapter("SELECT CustomerID, CompanyName FROM Customers", myconn);
+
+            myconn.Open();
+            myAdap.Fill(dtEmpresas);
+            myconn.Close();
+
+
+        }
+    }
+}
---
+++ Unidad.4.Lab.3.ADO.NetSQLyDataBinding/Ejercicio_6/Ejercicio_6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ejercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dtEmpresas = new DataTable();
+            dtEmpresas.Columns.Add("CustomerID", typeof(string));
+            dtEmpresas.Columns.Add("CompanyName", typeof(string));
+            SqlConnection myconn = new SqlConnection();
+            myconn.ConnectionString = "Data Source=LOCALHOST;Initial Catalog=Northwind; User ID=sa;Pwd=123";
+            SqlDataAdapter myAdap = new SqlDataAdapter("SELECT CustomerID, CompanyName FROM Customers", myconn);
+
+            myconn.Open();
+            myAdap.Fill(dtEmpresas);
+            myconn.Close();
+
+            Console.WriteLine("Escribe el CustomerID que desee modificar: ");
+            string custId = Console.ReadLine();
+            DataRow[] rowEmpresas = dtEmpresas.Select("CustomerID = '" + custId + "'");
+            if(rowEmpresas.Length != 1)
+            {
+                Console.WriteLine("CustomerID no encontrado");
+                Console.ReadLine();
+                return;
+            }
+
+            DataRow rowMiEmpresa = rowEmpresas[0];
+            string nombreActual = rowMiEmpresa["CompanyName"].ToString();
+            Console.WriteLine("Nombre actual de la empresa: " + nombreActual);
+            Console.WriteLine("Escriba el nuevo nombre: ");
+            string nombreNuevo = Console.ReadLine();
+            rowMiEmpresa.BeginEdit();
+            rowMiEmpresa["CompanyName"] = nombreNuevo;
+            rowMiEmpresa.EndEdit();
+
+            SqlCommand upCommand = new SqlCommand();
+            upCommand.Connection = myconn;
+            upCommand.CommandText = "UPDATE Customers SET CompanyName = @CompanyName WHERE CustomerID = @CustomerID";
+            upCommand.Parameters.Add("@CompanyName", SqlDbType.NVarChar, 50, "CompanyName");
+            upCommand.Parameters.Add("@CustomerID", SqlDbType.NVarChar, 5, "CustomerID");
+            myAdap.UpdateCommand = upCommand;
+            myAdap.Update(dtEmpresas);
+        }
+    }
+}
