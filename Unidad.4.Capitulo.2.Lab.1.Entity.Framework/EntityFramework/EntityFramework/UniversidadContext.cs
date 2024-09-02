36a100b6570be72159bd30c82d8d3c06a155a340
Mauro F. Giraudo <mfgiraudo456@gmail.com>
2024-09-02 00:36:14 -0300

    Unidad 4 Cap 2 Lab 1 Entity Framework TERMINADO

--- Unidad.4.Capitulo.2.Lab.1.Entity.Framework/EntityFramework/EntityFramework/UniversidadContext.cs
+++ Unidad.4.Capitulo.2.Lab.1.Entity.Framework/EntityFramework/EntityFramework/UniversidadContext.cs
@@ -11,11 +11,11 @@ namespace EntityFramework
     internal class UniversidadContext : DbContext
     {
         public DbSet<Alumno> Alumnos { get; set; }
-
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=MAUROG;Initial Catalog=Academia;Integrated Security=true;Trusted_Connection=true");
-            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+            optionsBuilder.UseSqlServer(@"Server=MAUROG;Initial Catalog=Academia;Integrated Security=true;Trusted_Connection=true;TrustServerCertificate=True");
+            //optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
         }
         public UniversidadContext()
         {
